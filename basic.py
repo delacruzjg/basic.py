cdcbfc6268d222395a363a074b1897e19f2e8f71
delacruzjg <157012706+delacruzjg@users.noreply.github.com>
2024-04-19 09:28:00 +0800

    basic.py

--- basic.py
+++ basic.py
@@ -11,14 +11,6 @@ def savings(gross_pay, tax_rate, expenses):
     '''Savings.
     5 points.
 
-    This function calculates the money remaining
-        for an employee after taxes and expenses.
-
-    To get the take-home pay of an employee, we will
-        follow the following process:
-        1. Apply the tax rate to the gross pay of the employee; round down
-        2. Subtract the expenses from the after-tax pay of the employee
-
     Parameters
     ----------
     gross_pay: int
@@ -45,17 +37,6 @@ import math
 def material_waste(total_material, material_units, num_jobs, job_consumption):
     '''Material Waste.
     5 points.
-
-    This function calculates how much material input will be wasted
-        after running a certain number of jobs that consume
-        a set amount of material.
-
-    To get the waste of a set of jobs:
-        1. Multiply the number of jobs by the material consumption per job.
-        2. Subtract the total material consumed from the total material available.
-
-    The users of this function also want you to format the output as a string, annotated with the
-        units in which the material is expressed. Do not add a space between the number and the unit.
 
     Parameters
     ----------
@@ -84,14 +65,6 @@ import math
 def interest(principal, rate, periods):
     '''Interest.
     5 points.
-
-    This function calculates the final value of an investment after
-        gaining simple interest over a number of periods.
-
-    To calculate simple interest, simply multiply the principal to the quantity (rate * time).
-        Add this amount to the principal to get the final value.
-
-    Round down the final amount.
 
     Parameters
     ----------
